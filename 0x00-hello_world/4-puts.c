08bdcd7421dd44e83e492520f352d146cdddaf96
Ehabw57 <maestro.ehab123@gmail.com>
2023-07-13 13:58:56 -0700

    this is a commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,7 +2,7 @@
 /* this funcution only prints the below text*/
 int main(void)
 {
-	char text[] = "Programming is like building a multilingual puzzle ";
+	char text[] = "\"Programming is like building a multilingual puzzle";
 
 	puts(text);
 	return (0);
