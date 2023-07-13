d4b8f9c10a113246fa37d5a99c538f5054c6d481
Ehabw57 <maestro.ehab123@gmail.com>
2023-07-13 14:28:46 -0700

    this is a commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
-/* main -  this funcution only prints the below text */
+/**
+ *  main -  this funcution only prints the below text
+ *
+ * Return: Always 0 (Success)
+*/
 int main(void)
 {
 	char text[] = "\"Programming is like building a multilingual puzzle ";
