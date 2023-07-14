3a2ae8a7418cc79a3441c127c113151bb410c3e7
Ehabw57 <maestro.ehab123@gmail.com>
2023-07-13 19:22:34 -0700

    this is a commit

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - A function that prints a single text2
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
