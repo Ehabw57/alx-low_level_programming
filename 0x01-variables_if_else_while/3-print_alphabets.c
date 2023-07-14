47648b6f8bb12530876a46c4199b82fb92e88bc2
Ehabw57 <maestro.ehab123@gmail.com>
2023-07-14 14:19:29 -0700

    this is a commit

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -11,17 +11,17 @@ int main(void)
 	int n = 97;
 	int z = 123;
 	int a = 65;
-        int x = 90;
+	int x = 90;
 
 	while (n != z)
 {
 	putchar(n);
 	n++;
 }
-        while (a != x)
+while (a <= x)
 {
-        putchar(a);
-        a++;
+	putchar(a);
+	a++;
 }
 putchar('\n');
 
