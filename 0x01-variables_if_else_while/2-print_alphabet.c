a5c4c1f0cdc78c7bb0a2e77f7f282cebcb3df8cf
Ehabw57 <maestro.ehab123@gmail.com>
2023-07-14 13:55:14 -0700

    this is a commit

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - is a function that prints a alphaber characters
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	int n = 97;
+	int z = 123;
+
+	while (n != z)
+{
+	putchar(n);
+	n++;
+}
+
+return (0);
+}
