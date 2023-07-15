d19dc3ee97569f5637e7836be0f7900991b68548
Ehabw57 <maestro.ehab123@gmail.com>
2023-07-15 04:01:26 -0700

    this is a commit

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - is a function that prints digits from 1 to 9 followed by  alphaber characters from a to f
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	int n = '0';
+	int z = '9';
+	char a = 'a';
+	char f = 'f';
+
+	while (n <= z)
+{
+	putchar(n);
+	n++;
+}
+while (a <= f)
+{
+	putchar(a);
+	a++;
+}
+putchar('\n');
+
+return (0);
+}
