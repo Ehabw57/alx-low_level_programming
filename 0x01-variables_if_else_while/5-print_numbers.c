79118d6564a9a1215930476e5068654f1613e32c
Ehabw57 <maestro.ehab123@gmail.com>
2023-07-15 02:07:35 -0700

    this is a commit

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - is a function that prints a alphaber characters
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	int n = 0;
+	int z = 10;
+
+	while (n != z)
+{
+	printf("%d", n);
+	n++;
+}
+putchar('\n');
+
+return (0);
+}
