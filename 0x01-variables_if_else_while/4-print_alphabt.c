a24c3e25df04208ae76d22f3549ece87d77774c1
Ehabw57 <maestro.ehab123@gmail.com>
2023-07-15 01:55:52 -0700

    this is a commit

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - is a function that prints a alphaber characters except e and q
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	int n = 97;
+	int z = 123;
+	int d = 100;
+	int q = 113;
+
+	while (n != z)
+{
+	if (n != d, n != q)
+	{
+		putchar(n);
+	}
+	n++;
+}
+putchar('\n');
+
+return (0);
+}
