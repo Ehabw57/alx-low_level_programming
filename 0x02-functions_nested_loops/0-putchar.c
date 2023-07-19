3a6a2b519fdc1057c70dccf45a39f73ec3277e45
Ehabw57 <maestro.ehab123@gmail.com>
2023-07-19 12:37:07 +0300

    this is a commit

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ *  * main - type a -putchar 
+ *   *
+ *    * Return: Always 0.
+ */
+
+int main (void)
+	putchr("_");
+	putchr("p");
+	putchr("u");
+	putchr("u");
+	putchr("c");
+	putchr("h");
+	putchr("a");
+	putchr("r");
+	putchr("\n");
+return (0);
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,3 @@
+hello this is my new folder for the nested loops project
+I will put all the solved tasks here 
+seeya :)
