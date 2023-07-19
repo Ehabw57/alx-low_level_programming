184597762484d4ddc8ebc2e6fd8db4bf57880338
Ehabw57 <maestro.ehab123@gmail.com>
2023-07-19 19:01:05 +0300

    added task 4 file

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _islower - sees if the inter num is an upperor lower case
+ * @c: is a parameter that recives a value from main funciton
+ * Return: ussally (0)
+ */
+
+int _isalpha(int c)
+{
+	if (c > 64)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,4 +1,5 @@
 void print_alphabet(void);
 int _putchar(char c);
 void print_alphabet_x10(void);
-int _islower(int c);
+int _islower(int c);i
+int _isalpha(int c);
