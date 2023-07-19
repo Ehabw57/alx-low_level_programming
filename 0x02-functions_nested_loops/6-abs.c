d9581d02487972da33ba544e75547c209b47a162
Ehabw57 <maestro.ehab123@gmail.com>
2023-07-20 00:43:17 +0300

    test 1

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_sign - sees if the intered method is bigger or lower or equal zero
+ * @n: is a parameter that recives a value from main funciton
+ * Return: ussally (0)
+ */
+
+int _abs(int)
+{
+	    if (int < 0)
+		        {
+				        return (-int);
+			}
+	        else
+			    {
+				            return (int);
+			    }
+}
