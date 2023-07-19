da04c9cca4f64c42ed6b7a91496c0103aec9433c
Ehabw57 <maestro.ehab123@gmail.com>
2023-07-19 15:35:32 +0300

    this is a commit

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _islower - sees if the inter num is an upper
+ * @c: is a parameter that recives a value from main
+ * or lower cace
+ * Return: ussally (0)
+ */
+
+int _islower(int c)
+{
+	if (c > 96)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,3 +1,4 @@
 void print_alphabet(void);
 int _putchar(char c);
 void print_alphabet_x10(void);
+int _islower(int c);
