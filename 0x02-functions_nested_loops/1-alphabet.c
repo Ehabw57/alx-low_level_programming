53e4b5c6dd9ef30056238c69ca70c4984be45ef9
Ehabw57 <maestro.ehab123@gmail.com>
2023-07-19 13:54:05 +0300

    this is a commit

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * main - prints the alphabet in lower case
+ *
+ * Return: Always 0
+ */
+
+void print_alphabet(void);
+{
+	char a = 'a'
+		for (a = 'a'; a <= 'z'; a++)
+		{
+			_putchar(a)
+}
+
+return (0);
+}
