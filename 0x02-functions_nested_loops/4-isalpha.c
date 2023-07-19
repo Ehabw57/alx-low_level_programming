75db99fa1361861d4700a3d6be4f7bf22e28bcfd
Ehabw57 <maestro.ehab123@gmail.com>
2023-07-19 23:50:58 +0300

    testo

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -12,12 +12,12 @@ int print_sign(int n)
 		_putchar('+');
 		return (1);
 	}
-	if else(n == 0) 
+	else if (n == 0) 
 	{
 		_putchar('0');
 		return (1);
 	}
-		else(n < 0)
+		else (n < 0);
 		{
 		_putchar('-');
 		return (-1);
