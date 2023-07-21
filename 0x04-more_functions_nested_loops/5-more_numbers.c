d27a4c9bc727f9b593ef8be8fbcbd84f56f9350c
Ehabw57 <maestro.ehab123@gmail.com>
2023-07-21 20:55:45 +0300

    fixed a bug

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -7,14 +7,14 @@ void more_numbers(void)
 {
 	int f = 0;
 	int i = 0;
-		while (f < 10)
+		while (f <= 10)
 		{
 			for (i = 0; i < 15; i++)
 			{
-				putchar(i / 10 + '0');
-				putchar(i % 10 + '0');
+				_putchar(i / 10 + '0');
+				_putchar(i % 10 + '0');
 			}
-			putchar('\n');
+			_putchar('\n');
 			f++;
 		}
 }
