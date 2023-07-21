9ffd6b212e058aa5f2a1f592e52a61a0d4b08fd1
Ehabw57 <maestro.ehab123@gmail.com>
2023-07-21 16:06:14 +0300

    uploded print_most_numbers function

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+* print_most_numbers - prints numbers from 0 to 9 ecvept 2 and 4
+*/
+void print_most_numbers(void)
+{
+	int f = 0;
+		while (f < 10)
+		{	
+			f++;
+			if (f = 2 || f = 4)
+			{
+				f++;
+			}
+			putchar(f + '0');
+		}
+		putchar('\n');
+}
