d3a45fe1ced0d472c24db4ac012aefe449ce00dd
Ehabw57 <maestro.ehab123@gmail.com>
2023-07-21 15:36:48 +0300

    uploded the mul function

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+* mul - checkes if c is a digit or somthing else
+* @a: the variable 1
+* @b: the variable 2
+* @r- the fainal result
+* Return: (mul)
+*/
+int mul(int a, int b)
+{
+	int r = a * b
+
+	return (r);
+}
