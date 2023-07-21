a946d0ee7abf4505c0c18f6d0d6ef931719ffb5f
Ehabw57 <maestro.ehab123@gmail.com>
2023-07-21 15:58:22 +0300

    uploded print numbers fuction

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+* print_numbers - prints numbers from 1 to 9
+*/
+void print_numbers(void)
+{
+	int f= 1;
+		while (f < 10)
+		{
+			putchar(f + '0');
+			f++;
+		}
+		putchar('\n');
+}
