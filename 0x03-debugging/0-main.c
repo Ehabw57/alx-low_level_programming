6c87062c6e2fb9df4656de8e146c7b870681f3dd
Ehabw57 <maestro.ehab123@gmail.com>
2023-07-21 11:24:24 +0300

    task 0 done

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,9 +1,15 @@
 #include "main.h"
 
+/**
+ * main - is a func to test the func print pos or neg
+ * Return: Always (0)
+ */
+
 int main(void)
 {
 	int i;
-		i= 0;
+		i = 0;
 	positive_or_negative(i);
+
 	return (0);
 }
