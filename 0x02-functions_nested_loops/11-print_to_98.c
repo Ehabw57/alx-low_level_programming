19e73d811e74ce20451867ae5f5fed9b26518256
Ehabw57 <maestro.ehab123@gmail.com>
2023-07-23 15:04:27 +0300

    uploded task 11 file

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_to_98 - is a function that prints every min of the day
+ * @n: the first num to add
+ **/
+void print_to_98(int n)
+{
+	int o = 98;
+
+	if (n >= 98)
+	{
+		for (o = 98; n > o; n--)
+		{
+			printf("%d, ",n);
+		}
+	}
+		if (n <= 98)
+		{
+			for (o = 98; n < o; n++)
+			{
+				printf("%d, ",n)
+			}
+		}
+		printf("%d\n");
+}
