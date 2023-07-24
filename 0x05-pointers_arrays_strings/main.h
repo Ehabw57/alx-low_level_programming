981c9722e913b2fde79557528725cda53531e909
Ehabw57 <maestro.ehab123@gmail.com>
2023-07-25 00:14:52 +0300

    header file issue

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,6 +1,6 @@
 void reset_to_98(int *n);
 #include <stdio.h>
-void swap_int(int *a, int *b):
+void swap_int(int *a, int *b);
 int _strlen(char *s);
 void _puts(char *str);
 void print_rev(char *s);
