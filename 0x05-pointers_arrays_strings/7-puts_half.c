de5c0b0d1e13f1fac89ee496774ed1653d7d0c13
Ehabw57 <maestro.ehab123@gmail.com>
2023-07-27 14:30:43 +0300

    try 3

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,29 +1,30 @@
 #include "main.h"
 /**
-* puts2 - is a function that puts a half of the line
-* @str: the line to reverse
-**/
+ *  * puts_half - prints the second half of a string
+ *   * @str: the string to print the second half of
+ *    */
 void puts_half(char *str)
 {
 	    int len = 0;
 	        char *r = str;
+		    int i;
 
-		  
-		    while (*str != '\0') {
-			            len++;
-				            str++;
-					        }
+		        /* Determine the length of the string */
+		        while (*str != '\0') {
+				        len++;
+					        str++;
+						    }
 
-
-		        int n = (len - 1) / 2;
-			    if (len % 2 == 0) {
-				            for (int i = n; i < len; i++) {
-						                putchar(*(r + i));
+			    /* Output the second half of the string */
+			    int n = (len - 1) / 2;
+			        if (len % 2 == 0) {
+					        for (i = n; i < len; i++) {
+							            putchar(*(r + i));
+								            }
+						    } else {
+							            for (i = n + 1; i < len; i++) {
+									                putchar(*(r + i));
+											        }
 								        }
-					        } else {
-							        for (int i = n + 1; i < len; i++) {
-									            putchar(*(r + i));
-										            }
-								    }
-			        putchar('\n');
+				    putchar('\n');
 }
