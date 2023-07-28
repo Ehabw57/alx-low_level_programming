e59a0dcdf1d7d9cc5a2d338585a27327c4fbe55d
Ehabw57 <maestro.ehab123@gmail.com>
2023-07-28 08:08:19 -0700

    uploded task 1 file

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _strcat - a function that copyes tow strings
+ * @dest: the one to copy text for
+ * @src: the one to copy text from
+ * @n: the index to copy
+ **/
+char *_strncat(char *dest, char *src, int n)
+{
+	int j;
+	int c;
+
+	for (i = 0; dest[i] != '\0'; i++)
+	{
+
+	}
+	for (c = 0; c <= n; c++)
+	{
+		    dest[i] = src[c];
+		    i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
