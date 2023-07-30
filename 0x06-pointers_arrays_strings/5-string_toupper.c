38e671731950ae9caca832eb7624a5bf5ebe1bc2
Ehabw57 <maestro.ehab123@gmail.com>
2023-07-30 14:46:50 +0300

    uploded task 5 file

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * *string_toupper - a function that convirets lower case char to
+ * upper case
+ * @str: the firest string
+ * Return: always str
+ **/
+char *string_toupper(char *str)
+{
+	int c;
+
+	for (c = 0; str[c] != '\0'; c++)
+	{
+		if (str[c] >= 'a' && str[c] <= 'z')
+		{
+			str[c] = str[c] - 32;
+		}
+		return (str);
+}
