3191edbc21782f70ff655e586cb518d862e493c0
Ehabw57 <maestro.ehab123@gmail.com>
2023-08-04 23:33:21 +0300

    uploded task 1 fiel

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - the function should print the num of arg
+ * that has been passed to it
+ * @c: the counter of  arg
+ * @s: the arh strings
+ * Return: always 0
+*/
+int main(int c, char *s[])
+{
+(void)s;
+printf("%i\n", c);
+return (0);
+}
