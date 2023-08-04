eda361f400ec233aacc6577e06f5cf35919b0d8f
Ehabw57 <maestro.ehab123@gmail.com>
2023-08-04 23:39:42 +0300

    uploded task 2 file

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - the function should print all of its passed arg 
+ * folloed by a newline
+ * @c: the counter of  arg
+ * @s: the argv string
+ * Return: always 0
+*/
+int main(int c, char *s[])
+{
+    int i;
+for (i = 0; i < c; i++)
+printf("%s\n", s[i]);
+return (0);
+}
