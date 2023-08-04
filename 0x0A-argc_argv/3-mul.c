01cb8315c2242de793719aba80b0d29bde487ec8
Ehabw57 <maestro.ehab123@gmail.com>
2023-08-04 23:57:53 +0300

    uploded task 3 file

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - the function should multi tow passed arg
+ * @c: the counter of  arg
+ * @s: the argv string
+ * Return: always 0
+ */
+int main(int c, char *s[])
+{
+c -= 1;
+if (c != 2)
+{
+printf("Error");
+}
+else
+{
+printf("%i\n", atoi(s[1]) * atoi(s[2]));
+}
+return (0);
+}
