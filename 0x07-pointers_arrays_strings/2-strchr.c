dd7489772b389b8436f5d549a0effd103db86ae2
Ehabw57 <maestro.ehab123@gmail.com>
2023-08-05 16:21:48 +0300

    fixed a bu

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,14 +1,13 @@
 #include "main.h"
 /**
- * *_strchr - is a function that search for a char in a strig
- * @s: the string
- * @c: the char to look for
- * Return: NULL if nothing is found or s if it is founf
+* *_strchr - is a function that search for a char in a strig
+* @s: the string
+* @c: the char to look for
+* Return: NULL if nothing is found or s if it is founf
 */
 
 char *_strchr(char *s, char c)
 {
-
 while (*s != '\0')
 {
 if (*s == c)
@@ -16,6 +15,10 @@ if (*s == c)
 return (s);
 }
 s++;
+}
+if (*s == c)
+{
+return (s);
 }
 return (NULL);
 }
