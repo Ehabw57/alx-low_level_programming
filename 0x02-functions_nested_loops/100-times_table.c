5f959c06e9bc88d9343ea9b64d60e29af5cc2fe7
Ehabw57 <maestro.ehab123@gmail.com>
2023-08-07 00:02:53 +0300

    uploded task 100 file

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,45 @@
+#include "main.h"
+
+/**
+* print_times_table - is a function that prints the full times table of n
+* @n: the variable to print its time table
+* Return: 0 if n is greater than 15 or less than 0
+**/
+void print_times_table(int n)
+{
+int i, j, r;
+
+if (n > 15 || n < 0)
+{
+return (0);
+}
+for (i = 0; i <= n; i++)
+{
+_putchar('0');
+for (j = 1; j <= n; j++)
+{
+r = i * j;
+_putchar(',');
+_putchar(' ');
+if (r < 10)
+{
+_putchar(' ');
+_putchar(' ');
+_putchar(r + '0');
+}
+else if (r < 100 && r > 9)
+{
+_putchar(' ');
+_putchar(r / 10 + '0');
+_putchar(r % 10 + '0');
+}
+else
+{
+_putchar(r / 100 + '0');
+_putchar((r / 10) * r % 10 + '0');
+_putchar(r % 10 + '0');
+}
+}
+putchar('\n');
+}
+}
--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,31 +1,31 @@
 #include "main.h"
 
 /**
- * times_table - is a function that prints every min of the day
- **/
+* times_table - is a function that prints every min of the day
+**/
 void times_table(void)
 {
-	int r, c, v;
+int r, c, v;
 
-	for (r = 0; r <= 9; r++)
-	{
-		putchar('0');
-		for (c = 1; c <= 9; c++)
-		{
-			v = r * c;
-			putchar(',');
-			putchar(' ');
-			if (r * c < 10)
-			{
-				putchar(' ');
-				putchar(v + '0');
-			}
-			else
-			{
-				putchar(v / 10 + '0');
-				putchar(v % 10 + '0');
-			}
-		}
-		putchar('\n');
-	}
+for (r = 0; r <= 9; r++)
+{
+putchar('0');
+for (c = 1; c <= 9; c++)
+{
+v = r * c;
+putchar(',');
+putchar(' ');
+if (r * c < 10)
+{
+putchar(' ');
+putchar(v + '0');
+}
+else
+{
+putchar(v / 10 + '0');
+putchar(v % 10 + '0');
+}
+}
+putchar('\n');
+}
 }
