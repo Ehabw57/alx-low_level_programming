f3b9c638ece424863f54b3daf2739a6476d67a08
Ehabw57 <maestro.ehab123@gmail.com>
2023-08-06 18:38:34 +0300

    updated

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,24 +1,24 @@
 #include "main.h"
 
 /**
- * print_last_digit - is a function that prints the
- * last digit og  variable f
- * @f: the variable to print its last digit
- * Return: Alwyas s
- **/
+* print_last_digit - is a function that prints the
+* last digit og  variable f
+* @n: the variable to print its last digit
+* Return: Alwyas s
+**/
 int print_last_digit(int n)
 {
-	int a;
+int a;
 
-	if (n < 0)
-		n = -n;
+if (n < 0)
+n = -n;
 
-	a = n % 10;
+a = n % 10;
 
-	if (a < 0)
-		a = -a;
+if (a < 0)
+a = -a;
 
-	_putchar(a + '0');
+_putchar(a + '0');
 
-	return (a);
+return (a);
 }
