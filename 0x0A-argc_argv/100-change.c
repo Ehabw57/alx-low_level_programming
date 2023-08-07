8e87e2397284f2f523ef7d8eb79ea8fe4c552c04
Ehabw57 <maestro.ehab123@gmail.com>
2023-08-07 18:06:56 +0300

    u

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,43 +1,49 @@
-
 #include <stdio.h>
 #include <stdlib.h>
 
 /**
-* main -  is a func that caculates amount of cents you need to give back
-* @argc: is the number of arg enterd
-* @argv: is  the omount of cents u mant to give back
-* Return: 1  if c is bigeecoins  ocoins less than 2
-**/
+ * main - calculates the minimum number of coins required to make change
+ * @argc: the number of arguments entered
+ * @argv: the amount of cents you want to give back
+ * Return: 1 if argc is not 2, 0 if coins is negative, otherwise 0
+ */
 int main(int argc, char *argv[])
 {
-int coins, counter;
+     int counter = 0;
+     int coins;
+  if (argc != 2)
+  {
+    printf("Error\n");
+    return 1;
+  }
 
-coins = atoi(argv[1]);
-if (argc != 2)
-{
-printf("Error\n");
-return (1);
-}
-if (coins <= 0)
-{
-printf("0\n");
-return (0);
-}
-while (coins > 0)
-{
-if (coins >= 25)
-coins -= 25;
-else if (coins >= 10)
-coins -= 10;
-else if (coins >= 5)
-coins -= 5;
-else if (coins >= 2)
-coins -= 2;
-else
-coins -= 1;
-counter++;
-}
-printf("%i\n", counter);
+   coins = atoi(argv[1]);
 
-return (0);
+  if (coins < 0)
+  {
+    printf("0\n");
+    return 0;
+  }
+
+
+
+  while (coins > 0)
+  {
+    if (coins >= 25)
+      coins -= 25;
+    else if (coins >= 10)
+      coins -= 10;
+    else if (coins >= 5)
+      coins -= 5;
+    else if (coins >= 2)
+      coins -= 2;
+    else
+      coins -= 1;
+
+    counter++;
+  }
+
+  printf("%i\n", counter);
+
+  return 0;
 }
