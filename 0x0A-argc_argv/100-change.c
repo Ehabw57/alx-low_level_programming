f241dfbd7a4d5597d611e5e74f8894dfbad63e11
Ehabw57 <maestro.ehab123@gmail.com>
2023-08-07 15:43:44 +0300

    testing chat gpt code

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,45 +1,39 @@
-
 #include <stdio.h>
 #include <stdlib.h>
 
 /**
- * main -  is a func that caculates amount of cents you need to give back
- * @argc: is the number of arg enterd
- * @argv: is  the omount of cents u mant to give back
- * Return: 1  if c is bigeecoins  ocoins less than 2
- **/
+ * main - Calculates the minimum number of coins required to make change for an amount of money.
+ * @argc: The number of command-line arguments.
+ * @argv: The array of command-line arguments.
+ * Return: 0 on success, 1 on error.
+ */
 int main(int argc, char *argv[])
 {
-    int coins, counter;
-
-    coins = atoi(argv[1]);
     if (argc != 2)
     {
-        printf("Error\n");
-        return (1);
+        printf("Error: Incorrect number of arguments\n");
+        return 1;
     }
-    if (coins > 0)
-  {  
-    while (coins > 0)
+
+    int cents = atoi(argv[1]);
+
+    if (cents <= 0)
     {
-        if (coins >= 25)
-            coins -= 25;
-        else if (coins >= 10)
-            coins -= 10;
-        else if (coins >= 5)
-            coins -= 5;
-        else if (coins >= 2)
-            coins -= 2;
-        else
-            coins -= 1;
-        counter++;
+        printf("Error: Invalid amount\n");
+        return 1;
     }
-    printf("%i\n", counter);
-  }
-  else 
-  {
-       printf("0\n");
-    return (0);
-  }
-  return (0);
+
+    int coins[] = {25, 10, 5, 2, 1};
+    int numCoins = sizeof(coins) / sizeof(coins[0]);
+    int count = 0;
+
+    for (int i = 0; i < numCoins; i++)
+    {
+        count += cents / coins[i];
+        cents %= coins[i];
+    }
+
+    printf("%d\n", count);
+
+    return 0;
 }
