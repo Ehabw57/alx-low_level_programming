3954589bfdc3e7b71ca31fd3b97e9bc7939e94d5
Ehabw57 <maestro.ehab123@gmail.com>
2023-08-07 15:47:37 +0300

    updated char gpt code

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -9,25 +9,26 @@
  */
 int main(int argc, char *argv[])
 {
-    if (argc != 2)
-    {
-        printf("Error: Incorrect number of arguments\n");
-        return 1;
-    }
-
-    int cents = atoi(argv[1]);
-
-    if (cents <= 0)
-    {
-        printf("Error: Invalid amount\n");
-        return 1;
-    }
-
+        int cents;
     int coins[] = {25, 10, 5, 2, 1};
     int numCoins = sizeof(coins) / sizeof(coins[0]);
     int count = 0;
+    int i;
+    if (argc != 2)
+    {
+        printf("Error\n");
+        return 1;
+    }
 
-    for (int i = 0; i < numCoins; i++)
+    cents = atoi(argv[1]);
+
+    if (cents <= 0)
+    {
+        printf("0\n");
+        return 0;
+    }
+
+    for (i = 0; i < numCoins; i++)
     {
         count += cents / coins[i];
         cents %= coins[i];
