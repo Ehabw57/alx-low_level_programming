9683c06733c3fa445a3b4282ea032608f3cc0588
Ehabw57 <maestro.ehab123@gmail.com>
2023-08-09 02:29:52 -0700

    this is a commit

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ *
+ *
+ *
+ **/
+char *_strdup(char *str)
+{
+	int counter = 0;
+	int i = 0;
+	char *ptr;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	while (*str != '\0')
+	{
+		str++;
+		counter++;
+	}
+	str -= counter;
+	ptr = malloc(sizeof(char) * counter;
+	if (ptr == NULL)
+	{
+	return (NULL);
+	}
+	for (; i <= counter; i++)
+	{
+	*(ptr + i) = *(str + i);
+	}
+	return (ptr);
+	free(ptr);
+}
