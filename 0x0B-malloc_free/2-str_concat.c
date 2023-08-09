fa3be9fc72c12f696077954e52163017679bc2bb
Ehabw57 <maestro.ehab123@gmail.com>
2023-08-09 09:29:41 -0700

    this is a commit

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -17,14 +17,22 @@ char *str_concat(char *s1, char *s2)
 	int size1, size2;
 	int i, j;
 	char *ptr;
-
-	if (s1 == NULL || s2 == NULL)
+	
+	if (s1 == NULL)
 	{
-		return (NULL);
+		s1 = "";
+	}
+	if (s2 == NULL)
+	{
+		s2 = "";
 	}
 	size1 = _strlen(s1);
 	size2 = _strlen(s2);
 	ptr = malloc(sizeof(char) * (size1 + size2 + 1));
+	if (ptr == NULL)
+	{
+		return (NULL);
+	}
 	for (i = 0; i < size1; i++)
 	{
 		ptr[i] = s1[i];
