9f2eecedddb7cbbc695435417012146763d4a8de
Ehabw57 <maestro.ehab123@gmail.com>
2023-08-09 01:21:31 -0700

    uplode task 0 file

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * *create_array - is a function that allocate some blocks in memroy
+ * @size: the size to be allocated
+ * @c: the char to put in every block
+ * Return: if size = 0 return NULL
+ * or return ptr
+ **/
+
+char *create_array(unsigned int size, char c)
+{
+	int i = 0;
+	char *ptr;
+
+	if (size == 0)
+	{
+		return (NULL);
+	}
+	ptr = malloc(sizeof(char) * size);
+	while (i <= size)
+	{
+		*(ptr + i) = 'H';
+		i++;
+	}
+	return (ptr);
+}
---
+++ 0x0B-malloc_free/README.md
@@ -0,0 +1,1 @@
+hello world it is my 7th week in \n alx have fun
