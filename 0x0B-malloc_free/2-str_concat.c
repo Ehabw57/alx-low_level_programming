fa18ae1a236f611bd0918e5e4fa157ac6dc11661
Ehabw57 <maestro.ehab123@gmail.com>
2023-08-09 10:06:35 -0700

    this is a commit

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,6 +1,11 @@
 #include "main.h"
 #include <stdlib.h>
 #include <stdio.h>
+/**
+ * _strlen - is afunction that caculates the lenth of a string
+ * @s1: the steing to find its lenth
+ * Return: always lenth
+ **/
 int _strlen(char *s1)
 {
 	int lenth;
@@ -11,13 +16,18 @@ int _strlen(char *s1)
 	}
 	return (lenth);
 }
-
+/**
+ * *str_concat - dublicate tow strings using malloc
+ * @s1: first string to dub
+ * @s2: sec string to dub
+ * Return: usually ptr
+ **/
 char *str_concat(char *s1, char *s2)
 {
 	int size1, size2;
 	int i, j;
 	char *ptr;
-	
+
 	if (s1 == NULL)
 	{
 		s1 = "";
