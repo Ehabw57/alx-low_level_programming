f93afd9add995fc6e6f1ca858b1ddd374cac0c25
Ehabw57 <maestro.ehab123@gmail.com>
2023-08-11 09:11:44 -0700

    forget to rename the file lol

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include "main.h"
+
+int *array_range(int min, int max)
+ {
+int *ptr;
+int i, j;
+if ( min > max)
+{
+    return (NULL);
+}
+ ptr = malloc(sizeof(int) * (max - min));
+ if (ptr == NULL)
+ {
+     return(NULL);
+ }
+j = 0; 
+  for (i = min; i < max; i++)
+  {
+      ptr[j] = i;
+      j++;
+  }
+  return (ptr);
+  }
