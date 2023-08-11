edd2d2991eee6996413058aa8b30e56e23a00a3a
Ehabw57 <maestro.ehab123@gmail.com>
2023-08-11 14:21:00 +0300

    updated the 1 task file

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -15,7 +15,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
  {
     unsigned int len1, len2;
    unsigned  int i, j;
-     void *ptr;
+     char *ptr;
  if (s1 == NULL)
  {
      s1 = "";
