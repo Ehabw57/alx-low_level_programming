d618e3a58f1442044687d425eb28e80bf7797fb1
Ehabw57 <maestro.ehab123@gmail.com>
2023-08-11 08:57:07 -0700

    uploded task 2 file

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,26 +1,32 @@
 #include <stdlib.h>
 #include "main.h"
 
+/**
+ * *_calloc - allocates size x nmemb memory block and set it to 0
+ * @nmemb: size 1
+ * @size: size 2
+ * Return: NULL if func fails or ptr2 if succes
+ */
 void *_calloc(unsigned int nmemb, unsigned int size)
- {
-     unsigned int i = 0;
-     void *ptr;
-     char *ptr2;
-     
-     if (size == 0 || nmemb == 0)
-     {
-     return (NULL);
-     }
-     ptr = malloc(size * nmemb);
-     if (ptr == NULL)
-     {
-         return (NULL);
-     }
-  ptr2  = (char*) ptr;
-     while (i < size * nmemb)
-     {
-         ptr2[i] = '\0'; 
-         i++;
-     }
-     return (ptr2);
- }
+{
+	unsigned int i = 0;
+	void *ptr;
+	char *ptr2;
+
+	if (size == 0 || nmemb == 0)
+	{
+		return (NULL);
+	}
+	ptr = malloc(size * nmemb);
+	if (ptr == NULL)
+	{
+		return (NULL);
+	}
+	ptr2  = (char *) ptr;
+	while (i < size * nmemb)
+	{
+		ptr2[i] = '\0';
+		i++;
+	}
+	return (ptr2);
+}
