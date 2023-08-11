3c2421b83580ebc9cad0186db6875414f1bb6083
Ehabw57 <maestro.ehab123@gmail.com>
2023-08-11 15:18:21 +0300

    upadted tasl 1 file ( memory alloction issue fixed

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,30 +1,32 @@
-#include "main.h"
 #include <stdlib.h>
 #include <string.h>
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
- {
-    unsigned int len1, len2;
- 
-     char *ptr;
- if (s1 == NULL)
- {
-     s1 = "";
- }
-  if (s2 == NULL)
- {
-     s2 = "";
- }
-     len1 = strlen(s1);
-     len2 = strlen(s2);
- if (n >= len2) 
- {
+ {  char *result;
+ unsigned int len1, len2;
+    if (s1 == NULL) {
+        s1 = ""; 
+    }
+
+    if (s2 == NULL) {
+        s2 = "";
+    }
+
+  len1 = strlen(s1);
+    len2 = strlen(s2);
+
+    if (n >= len2) {
         n = len2;
     }
-ptr = malloc(sizeof(char) * (len1 + n + 1));
- ptr = strncpy(ptr, s1, len1);
- ptr = strncpy(ptr + len1, s1, n);
-ptr[n + len1] ='\0';
-return (ptr);
- }
- 
+
+    result = malloc(sizeof(char) * (len1 + n + 1));
+    if (result == NULL) {
+        return NULL;
+    }
+
+    strncpy(result, s1, len1);
+    strncpy(result + len1, s2, n);
+    result[len1 + n] = '\0';
+
+    return result;
+}
