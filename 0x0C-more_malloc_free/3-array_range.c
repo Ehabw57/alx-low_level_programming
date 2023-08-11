03aa7fcee560998b0c245226f5f50cb8b1fd7d4b
Ehabw57 <maestro.ehab123@gmail.com>
2023-08-11 10:55:16 -0700

    mateches the file with betty codin and doc style

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -1,24 +1,30 @@
 #include <stdlib.h>
 #include "main.h"
-
+/**
+ * *array_range - rearnge a given ints of min and max
+ * @min: the first num to start from
+ * @max: the last num to reach
+ * Return: NULL if the func fails and ptr if success
+ */
 int *array_range(int min, int max)
- {
-int *ptr;
-int i, j;
-if ( min > max)
 {
-    return (NULL);
+	int *ptr;
+	int i, j;
+
+	if (min > max)
+	{
+		return (NULL);
+	}
+	ptr = malloc(sizeof(int) * (max - min + 1));
+	if (ptr == NULL)
+	{
+		return (NULL);
+	}
+	j = 0;
+	for (i = min; i < max; i++)
+	{
+		ptr[j] = i;
+		j++;
+	}
+	return (ptr);
 }
- ptr = malloc(sizeof(int) * (max - min + 1));
- if (ptr == NULL)
- {
-     return(NULL);
- }
-j = 0; 
-  for (i = min; i < max; i++)
-  {
-      ptr[j] = i;
-      j++;
-  }
-  return (ptr);
-  }
