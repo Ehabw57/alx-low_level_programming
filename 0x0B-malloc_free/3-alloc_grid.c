c81255b2876835a9985374d4ad580f3ca5e3cea6
Ehabw57 <maestro.ehab123@gmail.com>
2023-08-13 00:56:29 +0300

    trying to fix the memory leaks

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -10,7 +10,7 @@ int **alloc_grid(int width, int height)
     if (width < 1 || height < 1)
         return (NULL);
 
-    ptr = malloc(sizeof(int *) * height);
+    ptr = malloc(sizeof(int) * height);
 
     if (ptr == NULL)
     {
