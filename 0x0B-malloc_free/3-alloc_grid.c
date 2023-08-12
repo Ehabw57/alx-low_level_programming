b69727b133c404fe2b7547163313912024e2ab30
Ehabw57 <maestro.ehab123@gmail.com>
2023-08-13 00:39:43 +0300

    fixed a compile error

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -21,7 +21,7 @@ int **alloc_grid(int width, int height)
 
         for (j = 0; j < width; j++)
         {
-            ptr[i][j] == 0;
+            ptr[i][j] = 0;
         }
     }
     return (ptr);
