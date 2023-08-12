b10022e1c0c2251719cd91dd826de7180315c5ba
Ehabw57 <maestro.ehab123@gmail.com>
2023-08-12 10:33:01 -0700

    matched bettey coding and doc style

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -2,27 +2,39 @@
 #include <string.h>
 #include "main.h"
 
+/**
+ * check - this func cheks fot equlivent pointers
+ * @f: the stasted pointer
+ * @l: the late pointer
+ * Return: 1 if success 0 if fials
+ */
 int check(char *f, char *l)
 {
 	if (f >= l)
 		return (1);
 
 	if (*f == *l)
-		return check(f + 1, l - 1);
-	
+		return (check(f + 1, l - 1));
+
 	return (0);
 }
+/**
+ * is_palindrome - this func checks if s is a pointer to
+ * a plaindeome text or not
+ * @s: the pointer
+ * Return: always rectgion of  func check
+ */
 
 int is_palindrome(char *s)
 {
 	int len;
 	char *ptrl;
 	char *ptrf;
-	
+
 	ptrf = s;
 	ptrl = s;
 
 	len = strlen(s);
 	ptrl += len;
-	 return check(ptrf, ptrl - 1);
+	return (check(ptrf, ptrl - 1));
 }
