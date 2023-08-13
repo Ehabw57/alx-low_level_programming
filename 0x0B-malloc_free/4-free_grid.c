aba8c8be596228643b341e75ed28d36dad4cef74
Ehabw57 <maestro.ehab123@gmail.com>
2023-08-13 01:39:47 -0700

    mathched betty coding style

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -1,12 +1,17 @@
 #include "main.h"
 #include <stdlib.h>
+/**
+ * free_grid - free a2d gird
+ * @grid: the pointer to grid to be freed
+ * @height: the grid num of rows
+ */
 void free_grid(int **grid, int height)
 {
 int i;
 
-for(i = 0; i < height; i++)
+for (i = 0; i < height; i++)
 {
-    free(grid[i]);
+	free(grid[i]);
 }
-free (grid);
+free(grid);
 }
