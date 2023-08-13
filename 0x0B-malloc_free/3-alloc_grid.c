9574cdf5a3846d33afafd1309d082f5082fa8d8e
Ehabw57 <maestro.ehab123@gmail.com>
2023-08-13 01:04:36 -0700

    fixed memory alloction problem

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -4,31 +4,34 @@
 
 int **alloc_grid(int width, int height)
 {
-    int **ptr;
-    int i, j;
+	int **ptr;
+	int i, j;
+	
+	if (width < 1 || height < 1)
+	{
+		return (NULL);
+	}
+	
+	ptr = malloc(sizeof(int *) * height);
+	
+	if (ptr == NULL)
+	{
+		free(ptr);
+		return (NULL);
+	}
+	for (i = 0; i < height; i++)
+	{
+		ptr[i] = calloc(sizeof(int), width);
+		if (ptr[i] == NULL)
+		{
+			free(ptr);
 
-    if (width < 1 || height < 1)
-        return (NULL);
-
-    ptr = malloc(sizeof(int) * height);
-
-    if (ptr == NULL)
-    {
-        free(ptr);
-        return (NULL);
-    }
-
-    for (i = 0; i < height; i++)
-    {
-        ptr[i] = calloc(sizeof(int), width);
-
-        if (ptr[i] == NULL)
-        {
-            for (j = 0; j < i; j++)
-                free(ptr[j]);
-            free(ptr);
-            return NULL;
-        }
-    }
-    return (ptr);
+			for (j = 0; j < i; j++)
+			{
+				free(ptr[j]);
+			}
+			return (NULL);
+		}
+	}
+	return (ptr);
 }
