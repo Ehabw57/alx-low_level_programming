a0cb6e6a2e6508bb42804172e3dc83fd08e7f23d
Ehabw57 <maestro.ehab123@gmail.com>
2023-08-13 03:06:50 -0700

    uploded task 100 file

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,74 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _strlen - findes the lenth of the string
+ * @s: the string to find its lenth
+ * Return: always c
+ **/
+int _strlen(char *s)
+{
+        int c = 0;
+
+        while (*s > '\0')
+        {
+                s++;
+                c++;
+        }
+
+        return (c);
+}
+
+/**
+ * *_strcpy - this function copy the first
+ * string to the other one
+ * @dest: the one to copy text for
+ * @src: the one to copy text from
+ **/
+char *_strcpy(char *dest, char *src)
+{
+        int c;
+
+        for (c = 0; src[c] != '\0'; c++)
+        {
+                dest[c] = src[c];
+        }
+        dest[c] = '\0';
+
+        return (dest);
+}
+
+char *argstostr(int ac, char **av)
+{
+    int i, j;
+    int len = 0;
+    char *ptr;
+
+    if (ac == 0 || av == NULL)
+    {
+        return (NULL);
+    }
+
+    for (i = 0; i < ac - 1; i++)
+    {
+    len += _strlen(av[i]);
+    }
+
+    len += ac;
+    ptr = malloc(sizeof(char) * len);
+    
+    if (ptr == NULL)
+    {
+        return (NULL);
+    }
+    
+    for (i = 0, j = 0; i < ac ; i++)
+    {
+	  _strcpy(ptr + j, av[i]);
+	  j += _strlen(av[i]);
+	  ptr[j] = '\n';
+	  j++;
+    }
+    
+    return (ptr);
+}
