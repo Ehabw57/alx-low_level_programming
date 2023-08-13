ef6506ad82db438dd6f2193f371e12cb92af5b60
Ehabw57 <maestro.ehab123@gmail.com>
2023-08-13 01:19:06 -0700

    matched betty coding style

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -1,19 +1,23 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
-
+/**
+ * **alloc_grid - allociting memory for a 2d arr wirh 0
+ * @width: the num of columns
+ * @height: the num of rows
+ * Return: NUll if fails, ptr if succss
+ */
 int **alloc_grid(int width, int height)
 {
 	int **ptr;
 	int i, j;
-	
+
 	if (width < 1 || height < 1)
 	{
 		return (NULL);
 	}
-	
+
 	ptr = malloc(sizeof(int *) * height);
-	
 	if (ptr == NULL)
 	{
 		free(ptr);
