f02bd148869d6fe716198beaaa19b2a3658b7c7c
Ehabw57 <maestro.ehab123@gmail.com>
2023-08-15 00:31:43 +0300

    fixed a compile error

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
 /**
  * main - afunction that prints the name of the file its compiled from
-*/
-void main(void)
+ * Return: Always 0
+ */
+int main(void)
 {
 	printf("%s\n", __FILE__);
+
+	return (0);
 }
