79c41143c30e64c909216c5d3c8b4fe1bab427a6
Ehabw57 <maestro.ehab123@gmail.com>
2023-08-14 03:26:31 -0700

    Edited both "dog.h" and "1-init_dog.c" files
    mathched the both betyy coding and doc style and fixed some errors

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,8 +1,18 @@
 #include "dog.h"
+#include <stdlib.h>
 
+/**
+ * init_dog - it initalize the var my_dog with valus
+ * @d: apointer to struct dog
+ * @name: the value name
+ * @age: the value age
+ * @owner: the value owner
+ */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-    d->name = name;
-    d->age = age;
-    d->owner = owner;
+	if (d == NULL)
+		d = malloc(sizeof(struct dog));
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
 }
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,4 +1,15 @@
-struct dog {
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - is contating dog info
+ * @name: is the name of the dog
+ * @owner: is the name of the owner
+ * @age: dogs age
+ */
+
+struct dog
+{
 	char *name;
 	char *owner;
 	float age;
@@ -7,3 +18,4 @@ void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 typedef struct dog dog_t;
 dog_t *new_dog(char *name, float age, char *owner);
+#endif
