cf631528ec0b6264bc5bc773e11b41ae8597263b
Ehabw57 <maestro.ehab123@gmail.com>
2023-08-14 01:36:00 -0700

    made a new typdef "dog_t" in dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -4,3 +4,9 @@ struct dog {
 	float age;
 };
 void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+typedef struct dog {
+       char *name;
+       char *owner;
+       float age;
+}dog_t;
