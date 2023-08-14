357ffed7ba62d6d3159a32d63f0d123d6e1abca3
Ehabw57 <maestro.ehab123@gmail.com>
2023-08-15 00:29:37 +0300

    matched betty coding in doc style

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
-
+/**
+ * main - afunction that prints the name of the file its compiled from
+*/
 void main(void)
 {
-    printf("%s\n", __FILE__);
+	printf("%s\n", __FILE__);
 }
