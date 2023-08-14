487d2ea6060ad0f77798ba6c500803a3c7e5477d
Ehabw57 <maestro.ehab123@gmail.com>
2023-08-14 23:49:40 +0300

    uploded 5-free_dog.c file
    this will free all the dog yaaay

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+#include "dog.h"
+/**
+ * free_dog - free all the dog allocated memory Yaaaaaay
+ * @d: point to struct dog to be feed
+*/
+void free_dog(dog_t *d)
+{
+	free(d->name);
+	free(d->owner);
+	free(d);
+}
