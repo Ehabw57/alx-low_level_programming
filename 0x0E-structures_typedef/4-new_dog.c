a13c831df988cd5785f74323fe4627f072aaeae6
Ehabw57 <maestro.ehab123@gmail.com>
2023-08-14 23:01:07 +0300

    Fixed an issue in the output of 4-new_dog.c
    the was a problem in memory alloc the prog was allocatin ony enough memory for his members

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,6 +1,39 @@
 #include <stdlib.h>
+#include <stdio.h>
 #include "dog.h"
+/**
+ * _strlen - finds the lenth of a string
+ * @s: the pointer to string
+ * Return: Always len
+*/
+int _strlen(char *s)
+{
+	int len = 0;
 
+	while (*s == '\0')
+	{
+		s++;
+		len++;
+	}
+	return (len);
+}
+/**
+ * _strcpy - copyes a src to dest
+ * @dest: pointer to copy sting in
+ * @src: the sours to copy string from
+ * Return: Always dest
+*/
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	for (i = 0; src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+	dest[i] = '\0';
+	return (dest);
+}
 /**
  * *new_dog - a function that creates a new dog
  * @name: the vlaue name
@@ -10,16 +43,31 @@
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *ptr;
-
-	ptr = malloc(sizeof(dog_t));
+	dog_t *ptr = malloc(sizeof(dog_t));
 
 	if (ptr == NULL)
-	return (NULL);
+		return (NULL);
 
-	ptr->name = name;
+	ptr->name = malloc(_strlen(name));
+
+	if (ptr->name == NULL)
+	{
+		free(ptr);
+		return (NULL);
+	}
+
+	ptr->owner = malloc(_strlen(owner));
+
+	if (ptr->owner == NULL)
+	{
+		free(ptr->name);
+		return (NULL);
+	}
+
+	_strcpy(ptr->owner, owner);
+	_strcpy(ptr->name, name);
+
 	ptr->age = age;
-	ptr->owner = owner;
 
 	return (ptr);
 }
