950405f0283dda84523bf6b223f9ffce38222d3c
Ehabw57 <maestro.ehab123@gmail.com>
2023-08-25 22:51:06 +0300

    Removes uncceray files

--- 0x10-variadic_functions/3-task/main.c
+++
@@ -1,8 +0,0 @@
-#include "str.h"
-#include <stdio.h>
-int main(void)
-{
-    int r = zstrlen("cehg,cisfun");
-    printf("%d\n", r);
-    return 0;
-}
--- 0x10-variadic_functions/3-task/str.h
+++
@@ -1,6 +0,0 @@
-#ifndef STR_H
-#define STR_H
-#include <string.h>
-int zstrlen(char *src);
-
-#endif
