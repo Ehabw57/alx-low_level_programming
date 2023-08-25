6f8d6d22e78d5e5cddd4ee046820a1720e465e95
Ehabw57 <maestro.ehab123@gmail.com>
2023-08-25 15:06:28 +0300

    Reuploded task 4 file
    Addeed an if conditon it check if head is already null

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -2,21 +2,24 @@
 /**
  * free_list - Free all nodes in the linked list
  * @head: the head of the linked list
-*/
+ */
 void free_list(list_t *head)
 {
 	list_t *move;
 	list_t *Free;
 
-	move = head;
 	Free = head;
-
-	while (move->next != NULL)
+	if (head != NULL)
 	{
-		move = move->next;
-		free(Free->str);
-		free(Free);
-		Free = move;
+		move = head;
+
+		while (move->next != NULL)
+		{
+			move = move->next;
+			free(Free->str);
+			free(Free);
+			Free = move;
+		}
 	}
 	free(Free->str);
 	free(Free);
