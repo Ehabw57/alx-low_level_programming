f2fc9acd3816732bb909ffa57285f8a5a9a85c1a
Ehabw57 <maestro.ehab123@gmail.com>
2023-08-28 18:49:49 +0300

    Uploded task 1 file
    a function that returns how many nodes in the linked list

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+/**
+ * listint_len - a function that returns the number of nodes in the linked list
+ * @h: the head of the list
+ * Return: number of nodes in the list
+ */
+size_t listint_len(const listint_t *h)
+{
+	const listint_t *move;
+	int i;
+
+	if (h == NULL)
+	{
+		return (0);
+	}
+	move = h;
+	while (move != NULL)
+	{
+		move = move->next;
+		i++;
+	}
+	return (i);
+}
