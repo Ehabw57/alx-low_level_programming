a966da071eea6c482a71c9d43c32b9328c52f2e4
Ehabw57 <maestro.ehab123@gmail.com>
2023-08-29 16:09:24 +0300

    fixed insert_nodeint_at_index fucthion

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -10,7 +10,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	listint_t *move;
 
-	move = get_nodeint_at_index((*head), idx);
+	move = get_nodeint_at_index((*head), (idx - 1));
 
 	if (move == NULL)
 		return (NULL);
