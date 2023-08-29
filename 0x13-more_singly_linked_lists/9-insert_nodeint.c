492c0b004703b105fdb1835a81b2e60e25a4a72b
Ehab Hegazy <136044546+Ehabw57@users.noreply.github.com>
2023-08-29 19:36:00 +0300

    Update 9-insert_nodeint.c
    aded check case

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -7,7 +7,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	move = (*head);
 	new = malloc(sizeof(listint_t));
 
-	if (new == NULL)
+	if (new == NULL || head == NULL)
 		return (NULL);
 
 	new->n = n;
