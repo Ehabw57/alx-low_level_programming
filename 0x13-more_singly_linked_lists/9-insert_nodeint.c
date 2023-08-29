481a5686f397743a21668cd9e05a396fd937489b
Ehabw57 <maestro.ehab123@gmail.com>
2023-08-29 16:07:23 +0300

    fixed the new_node function

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -5,12 +5,12 @@
  * @idx: The given index
  * @n: the value must be addigend to new node
  * Return: the address of the new node
-*/
+ */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	listint_t *move;
 
-	move = get_nodeint_at_index((*head), (idx - 1));
+	move = get_nodeint_at_index((*head), idx);
 
 	if (move == NULL)
 		return (NULL);
@@ -23,13 +23,13 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
  * @head: the head of the list
  * @index: the index of the node u want to find
  * Return: a pointer to that node of the list
-*/
+ */
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 	listint_t *move;
 
 	move = head;
-	while (index != 0)
+	while (index > 0)
 	{
 		if (move == NULL)
 			return (NULL);
@@ -45,33 +45,25 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
  * @head: A pinter to the start of the list
  * @n: the value must be assigend to new node
  * Return: the addres of the new node
-*/
+ */
 listint_t *new_node(listint_t **head, int n)
 {
 	listint_t *new;
 
+	new = malloc(sizeof(listint_t));
+	if (new == NULL)
+		return (NULL);
+
+	new->n = n;
 	if ((*head) == NULL)
 	{
-		new = malloc(sizeof(listint_t));
-
-		if (new == NULL)
-			return (NULL);
-
-		new->n = n;
 		new->next = NULL;
-		(*head)->next = new;
+		(*head) = new;
 	}
-
 	else
 	{
-		new = malloc(sizeof(listint_t));
-		if (new == NULL)
-			return (NULL);
-
-		new->n = n;
 		new->next = (*head)->next;
 		(*head)->next = new;
 	}
-
 	return (new);
 }
