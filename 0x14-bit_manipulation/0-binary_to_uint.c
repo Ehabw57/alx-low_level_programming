b698293edb0e6cc2d1e93333d92c56565e6b33d3
Ehabw57 <maestro.ehab123@gmail.com>
2023-09-01 15:07:55 +0300

    Mateched betty doc style

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * binary_to _uint - converts binary to decimal
+ * binary_to_uint - converts binary to decimal
  * @b: pointer to string of 0 and 1
  * Return: the result in decimal if Sucess or 0 of Fails
 */
@@ -8,7 +8,7 @@ unsigned int binary_to_uint(const char *b)
 {
 	unsigned int result = 0;
 
-	while (*b)
+	while (b && *b)
 	{
 		if (*b == '0' || *b == '1')
 			result = (result << 1) | (*b - '0');
