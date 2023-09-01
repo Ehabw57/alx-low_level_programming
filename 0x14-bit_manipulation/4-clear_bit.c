d575d364183afa54631a4130365b234de97be87b
Ehabw57 <maestro.ehab123@gmail.com>
2023-09-01 23:25:44 +0300

    Uploded task 4 file

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * clear_bit - set a spacifc index bit to 0
+ * @n: pointer to intger
+ * @index: the index to set for
+ * Return: -1 if fails 1 if success
+*/
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+
+	if (index > sizeof(unsigned long int) * 8)
+		return (-1);
+
+	*n = (*n & ((~1) << index));
+	return (1);
+}
