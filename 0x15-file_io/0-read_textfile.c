76259125a7f74a400c67991d396e399dc6370627
Ehabw57 <maestro.ehab123@gmail.com>
2023-09-04 19:06:17 +0300

    reuploded task 0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,22 +1,28 @@
 #include "main.h"
+/**
+ * read_textfile - print a spacifc amount of chars of the given file to stdout
+ * @filename: the file to read from
+ * @letters: the amount of letters to read
+ * Return: the actual number of letters were printerd
+ */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-    int fd;
-    char c;
-    int b;
-    unsigned int count;
+	char *buf;
+	int fd;
+	unsigned int count;
 
-    fd = open(filename, O_RDONLY);
-    if (fd < 0 || filename == NULL)
-        return (0);
+	if (filename == NULL)
+		return (0);
 
-    while ((b = read(fd, &c, sizeof(c)) > 0) && count < letters)
-    {
-        b = write(1, &c, sizeof(c));
-        if (b < 0)
-        return(0);
-        count++;
-    }
-    close(fd);
-    return (count);
+	fd = open(filename, O_RDONLY);
+	buf = malloc(sizeof(char) * letters);
+
+	if (buf == NULL || fd < 1)
+		return (0);
+
+	count = read(fd, buf, letters);
+	write(1, buf, count);
+	free(buf);
+
+	return (count);
 }
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -2,6 +2,7 @@
 #define MAIN_H
 
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
 #include <fcntl.h>
 #include <string.h>
