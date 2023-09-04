d7e9c11da9878b98582a95fa208acf614f13ad58
Ehabw57 <maestro.ehab123@gmail.com>
2023-09-04 18:22:20 +0300

    Uploded task 0 file

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,20 @@
+#include "main.h"
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+    int fd;
+    char c;
+    int b;
+    unsigned int count;
+
+    fd = open(filename, O_RDONLY);
+    if (fd < 0)
+        return (0);
+
+    while ((b = read(fd, &c, sizeof(c)) > 0) && count < letters)
+    {
+        write(1, &c, sizeof(c));
+        count++;
+    }
+    close(fd);
+    return (count);
+}
---
+++ 0x15-file_io/main.h
@@ -0,0 +1,10 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <string.h>
+ssize_t read_textfile(const char *filename, size_t letters);
+
+#endif
