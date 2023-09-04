a8760274b53cc7f76e67ccb29dfac14eb7a43291
Ehabw57 <maestro.ehab123@gmail.com>
2023-09-04 23:53:47 +0300

    repluods

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -7,14 +7,20 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int fd;
+	int fd, byte;
 
 	if (filename == NULL)
 		return (-1);
+
 	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
-	if (fd < 0)
-		return (-1);
-	write(fd, text_content, sizeof(text_content));
+	if (text_content != NULL)
+	{
+		byte = write(fd, text_content, strlen(text_content));
+		if (fd < 0 || byte < 0)
+		{
+			return (-1);
+		}
+	}
 	close(fd);
 	return (1);
 }
