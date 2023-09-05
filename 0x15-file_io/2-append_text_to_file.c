ac8d739bb0739c6e6982b40b49f7c459f79c27b8
Ehabw57 <maestro.ehab123@gmail.com>
2023-09-05 15:06:32 +0300

    Uploded task 2 file
    a function that append new text contet to the file

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,15 +1,38 @@
 #include "main.h"
-/**
- * where filename is the name of the file
- * and text_content is the NULL terminated string to add at the end of the file
-Return: 1 on success and -1 on failure
-Do not create the file if it does not exist
-If filename is NULL return -1
-If text_content is NULL, do not add anything to the file.
- Return 1 if the file exists 
- and -1 if the file does not exist or if you do not have the required permissions to write the file
-*/
+
 int append_text_to_file(const char *filename, char *text_content)
 {
+    /**
+     * check if filename is not null
+     * int fd;
+     * fd = open(filename,o_append|Rw)
+     * check if fd is not -1
+     * if (text_cont != NULL)
+     * {
+     * wr = write(fd,text_content, stlenr(text_content))
+     * check wr if
+     * }
+     * return (1)
+     *
+     */
+    int fd, wr;
 
+    if (filename == NULL)
+        return (-1);
+
+    fd = open(filename, O_APPEND | O_WRONLY);
+    if (fd < 0)
+        return (-1);
+
+    if (text_content != NULL)
+    {
+        wr = write(fd, text_content, strlen(text_content));
+        if (wr < 0)
+        {
+            close(fd);
+            return (-1);
+        }
+    }
+    close(fd);
+    return (1);
 }
