0828bb31d68ecdb463b78663bde0bf2811d39754
Ehabw57 <maestro.ehab123@gmail.com>
2023-09-06 20:54:02 +0300

    uploded

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -15,7 +15,7 @@ void cp(char *file_from, char *file_to)
 			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
 			exit(98);
 		}
-		
+
 		if (fd2 < 0 || (w = write(fd2, buf, r)) < 0)
 		{
 			dprintf(STDERR_FILENO, "Error: Can't write to file %s\n", file_to);
@@ -25,16 +25,15 @@ void cp(char *file_from, char *file_to)
 	c1 = close(fd1);
 	if (c1 < 0)
 	{
-		dprintf(STDERR_FILENO, "Error: Cant close fd %i\n", fd1);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %i\n", fd1);
 		exit(100);
 	}
 	c2 = close(fd2);
 	if (c2 < 0)
 	{
-		dprintf(STDERR_FILENO, "Error: Cant close fd %i\n", fd2);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %i\n", fd2);
 		exit(100);
 	}
-
 }
 int main(int ac, char **av)
 {
