47845725a7edafc92ca345d335572f1d64328e2d
Ehabw57 <maestro.ehab123@gmail.com>
2023-09-06 20:05:53 +0300

    reuploded

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -7,30 +7,33 @@ int cp(char *file_from, char *file_to)
 
 	fd1 = open(file_from, O_RDONLY);
 	fd2 = open(file_to, O_WRONLY | O_CREAT | O_TRUNC, 0664);
-	while ((r = read(fd1, buf, 1024)) > 0)
+
+	while ((
+		
+	) > 0)
 	{
 		if (fd1 < 0 || r < 0)
 		{
-			dprintf(2, "Error: Can't read from %s\n", file_from);
+			dprintf(STDERR_FILENO, "Error: Can't read from %s\n", file_from);
 			exit(98);
 		}
-		w = write(fd2, buf, 1024);
+		w = write(fd2, buf, r);
 		if (fd2 < 0 || w < 0)
 		{
-			dprintf(2, " Error: Can't write to %s\n", file_to);
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_to);
 			exit(99);
 		}
 	}
 	c1 = close(fd1);
 	if (c1 < 0)
 	{
-		dprintf(2, "Error: Cant close %i\n", fd1);
+		dprintf(STDERR_FILENO, "Error: Cant close fd %i\n", fd1);
 		exit(100);
 	}
 	c2 = close(fd2);
 	if (c2 < 0)
 	{
-		dprintf(2, "Error: Cant close %i\n", fd2);
+		dprintf(STDERR_FILENO, "Error: Cant close fd %i\n", fd2);
 		exit(100);
 	}
 	return (0);
@@ -39,7 +42,7 @@ int main(int ac, char **av)
 {
 	if (ac != 3)
 	{
-		dprintf(2, "Usage: cp file_from file_to\n");
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
 	cp(av[1], av[2]);
