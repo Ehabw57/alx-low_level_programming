a161b268aa90c52ac56c658ed21fbf7f6ae35deb
Ehabw57 <maestro.ehab123@gmail.com>
2023-09-27 08:52:54 +0300

    Uploded leet function

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <fcntl.h>
+void _putchar(char s);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
@@ -6,3 +8,4 @@ int _strcmp(char *s1, char *s2);
 void reverse_array(int *a, int n);
 char *string_toupper(char *str);
 char *cap_string(char *sorce);
+char *leet(char *source);
