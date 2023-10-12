f91ebadd8d301daaf1f62dd2cab56e3232be949f
ehabw57 <maestor.ehab123@gmail.com>
2023-10-13 00:32:58 +0300

    keepdoing the hard work

---
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+/**
+ * delete_dnodeint_at_index - nonsense
+ * @head: not gonna worck on this now
+ * @index: good night
+ * Return: bye
+*/
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+{
+	return (index);
+}
