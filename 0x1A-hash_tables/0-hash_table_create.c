cc745feb1d00df5a5cc10e1cf89464d0a16806b8
Ehabw57 <maestro.ehab123@gmail.com>
2023-11-04 01:17:40 +0200

    Uploded the hash function

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -8,14 +8,25 @@
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *hash_table = malloc(sizeof(hash_node_t));
+	hash_table_t *hash_table = NULL;
+	unsigned long int index = 0;
+
+	hash_table = (hash_table_t *)malloc(sizeof(hash_table_t));
 
 	if (hash_table == NULL)
 		return (NULL);
 
-	hash_table->array = malloc(sizeof(hash_node_t) * size);
+	hash_table->size = size;
+
+	hash_table->array = (hash_node_t **)malloc(sizeof(hash_node_t) * size);
 	if (hash_table->array == NULL)
+	{
+		free(hash_table);
 		return (NULL);
+	}
+
+	for (index = 0; index < size; index--)
+		(hash_table->array)[index] = NULL;
 
 	return (hash_table);
 }
