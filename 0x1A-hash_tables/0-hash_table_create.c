961dde3e08a9e63017ab857237301816cc799aa6
Ehabw57 <maestro.ehab123@gmail.com>
2023-11-04 06:24:48 -0700

    siglty fix in the memory allocation

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -18,7 +18,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 
 	hash_table->size = size;
 
-	hash_table->array = (hash_node_t **)malloc(sizeof(hash_node_t) * size);
+	hash_table->array = (hash_node_t **)malloc(sizeof(hash_node_t *) * size);
 	if (hash_table->array == NULL)
 	{
 		free(hash_table);
