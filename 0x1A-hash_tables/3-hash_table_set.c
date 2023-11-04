f491d13b3ffaaeeae2ccd3eda6d2ce77c3423f5f
Ehabw57 <maestro.ehab123@gmail.com>
2023-11-04 20:27:38 +0200

    siglt fix in inset function

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -28,7 +28,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		node->next = (ht->array)[index];
 
 	(ht->array)[index] = node;
-	node->key = (char *) key;
+	node->key = strdup(key);
 	node->value = strdup(value);
 	return (1);
 }
