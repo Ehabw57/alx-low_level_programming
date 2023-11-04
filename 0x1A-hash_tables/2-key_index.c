a36180e46ec92805550184100affc2303913e1f4
Ehabw57 <maestro.ehab123@gmail.com>
2023-11-04 20:23:39 +0200

    Uploded insertation function

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -5,7 +5,7 @@
  * @size: Size of the hash table
  * Return: The index to store value
  */
-unsigned long int key_index(const char *key, unsigned long int size)
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
 	return (hash_djb2(key) % size);
 }
--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,36 +1,34 @@
 #include "hash_tables.h"
 #include <stdlib.h>
 #include <string.h>
+/**
+ * hash_table_set - Inserts a new value to the hash table
+ * @ht: The main hash table
+ * @key: The of the value
+ * @value: The value of the key
+ * Return: 1 if SUCCESS or 0 if FAILS
+*/
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	int index = 0;
 	hash_node_t *node = NULL;
 
+	if (ht == NULL || key == NULL || !strlen(key))
+		return (0);
 
-	index = key_index(key, ht->size);
+	index = key_index((const unsigned char *)key, ht->size);
+
+	node = (hash_node_t *)malloc(sizeof(hash_node_t));
+	if (node == NULL)
+		return (0);
 
 	if ((ht->array)[index] == NULL)
-	{
-		node = malloc(sizeof(hash_node_t));
-		if (node == NULL)
-			return (0);
 		node->next = NULL;
-	}
 	else
-	{
-		
-		node =  malloc(sizeof(hash_node_t));
-		if (node == NULL)
-			return (0);
 		node->next = (ht->array)[index];
-	}
 
 	(ht->array)[index] = node;
-	node->key = strdup(key);
+	node->key = (char *) key;
 	node->value = strdup(value);
 	return (1);
-
-
-
 }
-
