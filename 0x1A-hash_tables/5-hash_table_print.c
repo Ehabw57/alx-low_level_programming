f3b1b6a58c1f24680a1f2a755f667d3b3f41bb0e
Ehabw57 <maestro.ehab123@gmail.com>
2023-11-05 22:13:14 +0200

    sigtly mod the print function

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -10,21 +10,23 @@ void hash_table_print(const hash_table_t *ht)
 {
 	hash_node_t *move = NULL;
 	hash_node_t *print = NULL;
-	int flag = 0;
 	unsigned long int index = 0;
+	int printed = 0;
 
 	putchar('{');
 	for (move = (ht->array)[index]; index < ht->size; index++)
 	{
 		move = (ht->array)[index];
-		for (print = move; flag != 1 && print != NULL; print = print->next)
-		{
-			flag = 1;
-			printf("\'%s\': \'%s\'", print->key, print->value);
-			break;
-		}
 		for (print = move; print != NULL; print = print->next)
+		{
+			if (printed == 0)
+			{
+				printf("\'%s\': \'%s\'", print->key, print->value);
+				printed += 1;
+				continue;
+			}
 			printf(", \'%s\': \'%s\'", print->key, print->value);
+		}
 	}
 	printf("}\n");
 }
