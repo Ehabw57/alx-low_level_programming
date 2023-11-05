c9ca7cd0e791f9b8ba2cffe2129dc53f7ea2b931
Ehabw57 <maestro.ehab123@gmail.com>
2023-11-05 13:19:15 +0200

    New refator for the insert function

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -6,41 +6,48 @@
  * @ht: The main hash table
  * @key: The of the value
  * @value: The value of the key
- * Return: 1 if SUCCESS or 0 if FAILS
+ * Return: 1 if SUCCESS or 0 if somthing went wrong
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	int index = 0;
-	hash_node_t *node = NULL, *move = NULL;
+	hash_node_t *node = NULL;
 
 	if (ht == NULL || key == NULL || !strlen(key))
 		return (0);
 
 	index = key_index((const unsigned char *)key, ht->size);
-
-	node = (hash_node_t *)malloc(sizeof(hash_node_t));
-	if (node == NULL)
-		return (0);
-
-	if ((ht->array)[index] == NULL)
-		node->next = NULL;
-	else
+	/*Start searching for key in the table*/
+	for (node = (ht->array)[index]; node != NULL; node = node->next)
 	{
-		for (move = (ht->array)[index]; move != NULL; move = move->next)
+		if (strcmp(key, node->key) == 0)
 		{
-			if (strcmp(key, move->key))
-			{
-				free(move->value);
-				free(node);
-				move->value = strdup(value);
-				return (1);
-			}
+			free(node->value);
+			node->value = strdup(value);
+			if (node->value == NULL)
+				return (0); /*Faild to duplicate value*/
+			return (1);		/*Key found and value updated*/
 		}
-		node->next = (ht->array)[index];
 	}
 
-	(ht->array)[index] = node;
+	/*Either key not found or index got no collision */
+	node = (hash_node_t *)malloc(sizeof(hash_node_t)); /*New node allocated*/
+	if (node == NULL)
+		return (0); /*Could not allocate new node*/
+
+	/*Store the value and key*/
 	node->key = strdup(key);
 	node->value = strdup(value);
+	if (node->value == NULL || node->key == NULL)
+	{
+		free(node->value);
+		free(node->key);
+		free(node);
+		return (0); /*Could not duplicate key or value*/
+	}
+	/*Stack the new node*/
+	node->next = (ht->array)[index];
+	(ht->array)[index] = node;
+
 	return (1);
 }
