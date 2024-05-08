11bf8353bd3071240caaf3f0a963409df79c26a6
Ehabw57 <maestor.ehab123@gmail.com>
2024-05-08 22:49:49 +0300

    all text answers

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -1,5 +1,5 @@
-#ifndef  SEARCH_ALGOS
-#define  SEARCH_ALGOS
+#ifndef SEARCH_ALGOS
+#define SEARCH_ALGOS
 #include <stdio.h>
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
